f79882a58672a11110feab81c7a7b2e2847c1f0e
Hideo Oshima <orimajp@gmail.com>
2019-12-08 17:20:04 +0900

    ./gradlew buildでテスト時にアプリケーションクラスが見つからずエラーになる問題に対応

---
+++ multi-project-infrastructure/src/test/java/com/example/multiprojectmain/infrastructure/persistence/repository/message/MessageRepositoryImplTest.java
@@ -0,0 +1,37 @@
+package com.example.multiprojectmain.infrastructure.persistence.repository.message;
+
+import com.example.multiprojectmain.MultiProjectMainApplication;
+import com.example.multiprojectmain.domain.message.Message;
+import com.example.multiprojectmain.domain.message.MessageRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = MultiProjectMainApplication.class)
+class MessageRepositoryImplTest {
+
+    @Resource
+    MessageRepository messageRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test() {
+        Optional<Message> messageResponse = messageRepository.getMessage(1);
+        assertTrue(messageResponse::isPresent);
+    }
+
+}
---
+++ multi-project-main/src/test/java/com/example/multiprojectmain/application/command/service/message/MessageCommandServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.multiprojectmain.application.command.service.message;
+
+import com.example.multiprojectmain.MultiProjectMainApplication;
+import com.example.multiprojectmain.application.command.response.message.UpdateMessageResponse;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = MultiProjectMainApplication.class)
+class MessageCommandServiceTest {
+
+    @Resource
+    MessageCommandService messageCommandService;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void test() {
+        UpdateMessageResponse response = messageCommandService.getUpdateMessage(1);
+        assertEquals("こんにちは。", response.getMessage());
+    }
+
+}
